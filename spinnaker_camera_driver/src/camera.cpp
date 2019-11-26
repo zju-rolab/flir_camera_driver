46f4acb07b99a917e2bca94299d38a27e6b92727
dawnos <litang@gmail.com>
2019-11-26 15:50:34 +0100

    TriggerOverlap mode support.

--- spinnaker_camera_driver/src/camera.cpp
+++ spinnaker_camera_driver/src/camera.cpp
@@ -94,6 +94,7 @@ void Camera::setNewConfiguration(const SpinnakerConfig& config, const uint32_t& 
     setProperty(node_map_, "TriggerSelector", config.trigger_selector);
     setProperty(node_map_, "TriggerActivation", config.trigger_activation_mode);
     setProperty(node_map_, "TriggerMode", config.enable_trigger);
+    setProperty(node_map_, "TriggerOverlap", config.trigger_overlap_mode);
 
     setProperty(node_map_, "LineSelector", config.line_selector);
     setProperty(node_map_, "LineMode", config.line_mode);
